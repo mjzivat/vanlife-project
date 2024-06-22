aaaa1c738e4b5f7554af442411f6eff66beb602f
mjzivat <mjzivat@gmail.com>
2024-06-22 16:32:20 -0500

    added deps, initialized firebase

--- api.js
+++ api.js
@@ -1,4 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCA8FDmtl5OTzsyIYcXSgw2wT-2o658Cd4",
+    authDomain: "van-life-c98f2.firebaseapp.com",
+    projectId: "van-life-c98f2",
+    storageBucket: "van-life-c98f2.appspot.com",
+    messagingSenderId: "236271472691",
+    appId: "1:236271472691:web:bfb0898350af42fe37d346"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app)
+
 export async function getVans(id) {
+
+    
+
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
     if (!res.ok) {
