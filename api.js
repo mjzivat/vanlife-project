d1a1a506756fe2b02fbc44b3decdad17acda7b36
mjzivat <mjzivat@gmail.com>
2024-06-22 17:19:53 -0500

    refactored getVans function

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore/lite"
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
 
 const firebaseConfig = {
     apiKey: "AIzaSyCA8FDmtl5OTzsyIYcXSgw2wT-2o658Cd4",
@@ -17,22 +17,31 @@ const firebaseConfig = {
 
 
 
-export async function getVans(id) {
+  export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    console.log(vans)
 
-    
-
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+    return vans
 }
+
+// export async function getVans(id) {
+
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
