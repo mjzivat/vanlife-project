acac2f5e044ec4fd6f91213858db4bf35df17eb3
mjzivat <mjzivat@gmail.com>
2024-06-22 21:22:12 -0500

    added getHostVans function

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, getDoc, getDocs } from "firebase/firestore/lite"
+import { getFirestore, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore/lite"
 
 const firebaseConfig = {
     apiKey: "AIzaSyCA8FDmtl5OTzsyIYcXSgw2wT-2o658Cd4",
@@ -51,19 +51,16 @@ export async function getVan(id) {
 //     return data.vans
 // }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
 }
+
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
